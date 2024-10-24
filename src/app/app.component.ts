c3ebcf3ba1f23aa7c43f30e4f907fb7011888483
srujan1306 <srujan.pothu@proclink.com>
2024-10-24 17:58:35 +0530

    form open in url is set successfully

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { EmailSignatureComponent } from './email-signature/email-signature.component';
+import { EmployeeFormComponent } from './employee-form/employee-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, EmailSignatureComponent, RouterLink],
+  imports: [
+    RouterOutlet,
+    EmailSignatureComponent,
+    RouterLink,
+    EmployeeFormComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
